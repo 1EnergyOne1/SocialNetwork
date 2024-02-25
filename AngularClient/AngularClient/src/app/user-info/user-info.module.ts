9538e6bedd04ec5d185a777251d7c57ac65f56da
EnergyOne <energyone.tony@mail.ru>
2024-02-26 00:12:06 +0300

    Создан клиент на ангуляре

---
+++ AngularClient/AngularClient/src/app/user-info/user-info.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { UserInfoComponent } from './user-info.component';
+
+@NgModule({
+  declarations: [
+    UserInfoComponent    
+  ],
+  imports: [
+  ],
+  exports: [UserInfoComponent]
+})
+export class UserInfoModule { }
