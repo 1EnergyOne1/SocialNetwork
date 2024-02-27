3fc9f16c011de498d858dda08b0d3d9a5f3ebffb
EnergyOne <energyone.tony@mail.ru>
2024-02-28 00:13:39 +0300

    создание окна в WPF страницы пользователя

---
+++ WPFClient/vm/UserVM.cs
@@ -0,0 +1,14 @@
+﻿using Api.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClient.vm
+{
+    public class UserVM
+    {
+        public User Data { get; set; }
+    }
+}
