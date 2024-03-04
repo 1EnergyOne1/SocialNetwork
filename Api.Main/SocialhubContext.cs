31581955a3247c728abb96e72236f9703e1a8ff9
EnergyOne <energyone.tony@mail.ru>
2024-03-04 23:47:45 +0300

    применён скрипт

--- Api.Main/SocialhubContext.cs
+++ Api.Main/SocialhubContext.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using Api.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Main;
 
 public partial class SocialhubContext : DbContext
 {
-    public DbSet<User> User { get; set; }
     public SocialhubContext()
     {
     }
@@ -17,12 +15,32 @@ public partial class SocialhubContext : DbContext
     {
     }
 
+    public virtual DbSet<User> Users { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=socialhub;Username=postgres;Password=Veo48764511", x => x.UseNodaTime());
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Datecreate).HasColumnName("datecreate");
+            entity.Property(e => e.Isadmin).HasColumnName("isadmin");
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(30)
+                .HasColumnName("lastname");
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .HasColumnName("name");
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
---
+++ Api.Main/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using NodaTime;
+
+namespace Api.Main;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Lastname { get; set; }
+
+    public int? Age { get; set; }
+
+    public LocalDateTime? Datecreate { get; set; }
+
+    public bool? Isadmin { get; set; }
+}
